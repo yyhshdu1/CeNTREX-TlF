78a472ad3088bd49f41bb96591bb8d03d5a0a917
ograsdijk <o.grasdijk@gmail.com>
2021-08-06 20:45:35 -0400

    implement __eq__ for State

--- centrex_TlF/states/states.py
+++ centrex_TlF/states/states.py
@@ -422,6 +422,8 @@ class State:
         h = tuple(a*s.__hash__() for a,s in self)
         return int(hashlib.md5(json.dumps(h).encode()).hexdigest(),16)
 
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
     
     # direct access to a component
     def __getitem__(self, i):
