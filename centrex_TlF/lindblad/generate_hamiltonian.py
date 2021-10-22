4b20a3d7eed6fc34f8a29fb0bb18694f381e9d8b
ograsdijk <o.grasdijk@gmail.com>
2021-10-22 18:12:23 -0500

    Add support for the TransitionSelector for generating the symbolic hamiltonian

--- centrex_TlF/lindblad/generate_hamiltonian.py
+++ centrex_TlF/lindblad/generate_hamiltonian.py
@@ -1,5 +1,6 @@
 import copy
 import logging
+from centrex_TlF.couplings.utils import TransitionSelector
 import numpy as np
 from sympy import zeros, Symbol
 from centrex_TlF.couplings import (
@@ -113,6 +114,21 @@ def generate_symbolic_detunings(n_states, detunings):
 
 def generate_total_symbolic_hamiltonian(QN, H_int, couplings, transitions,
                                         slice_compact = None, qn_compact = None):
+    if isinstance(transitions[0], TransitionSelector):
+        return generate_total_symbolic_hamiltonian_TransitionSelector(
+                                            QN, H_int, couplings, transitions,
+                                        slice_compact = None, qn_compact = None
+                                        )
+    elif isinstance(transitions[0], dict):
+        return generate_total_symbolic_hamiltonian_transitiondict(
+                                            QN, H_int, couplings, transitions,
+                                        slice_compact = None, qn_compact = None
+                                        )
+    else:
+        raise AssertionError("transitions required to be a list of TransitionSelectors or a list of dicts")
+
+def generate_total_symbolic_hamiltonian_transitiondict(QN, H_int, couplings, transitions,
+                                        slice_compact = None, qn_compact = None):
     """Generate the total symbolic hamiltonian for the given system
 
     Args:
@@ -145,6 +161,56 @@ def generate_total_symbolic_hamiltonian(QN, H_int, couplings, transitions,
             pols.append(None)
         else:
             pols.append(transition['polarization symbols'])
+
+    H_symbolic = generate_symbolic_hamiltonian(QN, H_rot, couplings, Ωs, Δs, 
+                                                                        pols)
+
+    if slice_compact:
+        H_symbolic = delete_row_column_symbolic(H_symbolic, slice_compact)
+    elif qn_compact:
+        QN_compact = copy.deepcopy(QN)
+        for qnc in qn_compact:
+            indices_compact = get_indices_quantumnumbers(qnc, QN_compact)
+            QN_compact = compact_QN_coupled_indices(QN_compact, indices_compact)
+            H_symbolic = compact_symbolic_hamiltonian_indices(H_symbolic, indices_compact)
+        return H_symbolic, QN_compact
+
+    return H_symbolic
+
+def generate_total_symbolic_hamiltonian_TransitionSelector(QN, H_int, couplings, transitions,
+                                        slice_compact = None, qn_compact = None):
+    """Generate the total symbolic hamiltonian for the given system
+
+    Args:
+        QN (list): states
+        H_int (array): internal hamiltonian
+        couplings (list): list of dictionaries with all couplings of the system
+        transitions (list): list of dictionaries with all transitions of the 
+                            system
+        slice_compact (slice operator, optional): numpy slice operator for which 
+                                                    part of the system to compact 
+                                                    to a single state. 
+                                                    Defaults to None.
+        qn_compact (list, optional): list of QuantumSelectors or lists of
+                                    QuantumSelectors with each 
+                                    QuantumSelector containing the quantum 
+                                    numbers to compact into a single state. 
+                                    Defaults to None.
+
+    Returns:
+        sympy matrix: symbolic hamiltonian
+        if qn_compact is provided, also returns the states corresponding to the 
+        compacted hamiltonian, i.e. ham, QN_compact
+    """
+    H_rot = generate_total_hamiltonian(H_int, QN, couplings)
+    Ωs = [t.Ω for t in transitions]
+    Δs = [t.δ for t in transitions]
+    pols = []
+    for transition in transitions:
+        if not transition.polarization_symbols:
+            pols.append(None)
+        else:
+            pols.append(transition.polarization_symbols)
 
     H_symbolic = generate_symbolic_hamiltonian(QN, H_rot, couplings, Ωs, Δs, 
                                                                         pols)
