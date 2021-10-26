9e97f33f162064747955b309835c6220d43fb4ad
ograsdijk <o.grasdijk@gmail.com>
2021-10-26 14:47:58 -0400

    move generate_preamble to generate_julia_code.py

--- centrex_TlF/lindblad/generate_julia_code.py
+++ centrex_TlF/lindblad/generate_julia_code.py
@@ -5,8 +5,27 @@ from centrex_TlF.lindblad.utils_multiprocessing import (
 )
 
 __all__ = [
-    'system_of_equations_to_lines'
+    'system_of_equations_to_lines',
+    'generate_preamble'
 ]
+
+def generate_preamble(odepars: odeParameters, transitions: list) -> str:
+    # check if the symbols in transitions are defined by odepars
+    odepars.check_transition_symbols(transitions)
+    preamble = """function Lindblad_rhs!(du, ρ, p, t)
+    \t@inbounds begin
+    """
+    for idp, par in enumerate(odepars._parameters):
+        preamble += f"\t\t{par} = p[{idp+1}]\n"
+    for par in odepars._compound_vars:
+        preamble += f"\t\t{par} = {getattr(odepars, par)}\n"
+        
+    for transition in transitions:
+        preamble += f"\t\t{transition.Ω}ᶜ = conj({transition.Ω})\n"
+        
+    # remove duplicate lines (if multiple transitions have the same Rabi rate symbol or detuning
+    preamble = "\n".join(list(OrderedDict.fromkeys(preamble.split("\n"))))
+    return preamble
 
 def system_of_equations_to_lines(system, nprocs = 1):
     n_states = system.shape[0]
--- centrex_TlF/lindblad/utils_julia.py
+++ centrex_TlF/lindblad/utils_julia.py
@@ -11,7 +11,7 @@ __all__ = [
     'odeParameters', 'setup_parameter_scan_1D', 'setup_ratio_calculation',
     'setup_initial_condition_scan', 'setup_state_integral_calculation',
     'get_indices_diag_flattened', 'setup_parameter_scan_ND', 
-    "handle_randomized_ensemble_solution", "generate_preamble"
+    "handle_randomized_ensemble_solution"
 ]
 
 def initialize_julia(nprocs):
@@ -246,23 +246,7 @@ def setup_parameter_scan_ND(odePar, parameters, values, randomize = False):
     if randomize:
         return ind_random
 
-def generate_preamble(odepars: odeParameters, transitions: list) -> str:
-    # check if the symbols in transitions are defined by odepars
-    odepars.check_transition_symbols(transitions)
-    preamble = """function Lindblad_rhs!(du, ρ, p, t)
-    \t@inbounds begin
-    """
-    for idp, par in enumerate(odepars._parameters):
-        preamble += f"\t\t{par} = p[{idp+1}]\n"
-    for par in odepars._compound_vars:
-        preamble += f"\t\t{par} = {getattr(odepars, par)}\n"
-        
-    for transition in transitions:
-        preamble += f"\t\t{transition.Ω}ᶜ = conj({transition.Ω})\n"
-        
-    # remove duplicate lines (if multiple transitions have the same Rabi rate symbol or detuning
-    preamble = "\n".join(list(OrderedDict.fromkeys(preamble.split("\n"))))
-    return preamble
+
 
 def setup_ratio_calculation(states):
     cmd = ""
