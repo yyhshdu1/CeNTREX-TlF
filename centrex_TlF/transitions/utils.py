3269e356f7c2f04e14ff7081d3f92a7ae05eb2d2
ograsdijk <o.grasdijk@gmail.com>
2022-02-10 00:37:59 -0600

    Fix error in `check_transitions_allowed_coupled` and made `construct_ground_states_allowed`
    `construct_ground_states_allowed` creates all allowed ground states given an excited state and coupling ΔmF

--- centrex_TlF/transitions/utils.py
+++ centrex_TlF/transitions/utils.py
@@ -1,8 +1,63 @@
 import numpy as np
+from centrex_TlF.states.utils import QuantumSelector
+from centrex_TlF.states.generate_states import (
+    generate_coupled_states_ground_X,
+    generate_coupled_states_excited_B
+)
 
 __all__ = [
-    "check_transition_coupled_allowed", "assert_transition_coupled_allowed"
+    "check_transition_coupled_allowed", "assert_transition_coupled_allowed",
+    "construct_ground_states_allowed"
 ]
+
+def construct_ground_states_allowed(Jg, Je, F1, F, mF = None, P = None, ΔmF = None):
+    """Construct the coupled ground states given a ground state J and excited
+    state quantum numbers J', F1', F' and optionally mF'
+
+    Args:
+        Jg (int): rotational ground state
+        Je (int): rotational excited state
+        F1 (float): excited F1 state
+        F (int): excited F state
+        mF (int, optional): excited mF state. Defaults to None, which includes 
+                            all excited state mF.
+        P (int, optional): excited state parity. Defaults to None, which picks
+                            the allowed excited state parity given the ground
+                            state.
+        ΔmF (int, optional): ΔmF of transition. Defaults to None, which checks
+                            for all ΔmF.
+
+    Returns:
+        np.ndarray: np.ndarray of allowed ground states as CoupledBasisStates
+    """
+    if P is None:
+        P = (-1)**(Jg+1)
+    if ΔmF is None:
+        ΔmF = [-1,0,1]
+    if not isinstance(ΔmF, (list, tuple, np.ndarray)):
+        ΔmF = [ΔmF]
+    # generate states
+    ground = QuantumSelector(J = Jg, electronic = 'X', P = (-1)**Jg)
+    excited = QuantumSelector(J = Je, F1 = F1, F = F, mF = mF, electronic='B', P = P)
+    ground = generate_coupled_states_ground_X(ground)
+    excited = generate_coupled_states_excited_B(excited)
+
+    # brute force loop over states to find allowed ground states
+    ground_allowed = []
+    for idg, g in enumerate(ground):
+        for e in excited:
+            for Δ in ΔmF:
+                if check_transition_coupled_allowed(
+                        g,e, ΔmF_allowed = Δ, return_err=False
+                    ):
+                    ground_allowed.append(idg)
+    ground_allowed = np.unique(ground_allowed)
+
+    if len(ground_allowed) == 0:
+        return []
+    else:
+        return ground[ground_allowed]
+
 
 def check_transition_coupled_allowed(state1, state2, ΔmF_allowed, 
                                     return_err = True):
@@ -16,15 +71,14 @@ def check_transition_coupled_allowed(state1, state2, ΔmF_allowed,
     Returns:
         tuple: (allowed boolean, error message)
     """
-    Fg = state1.F
     ΔF = int(state2.F - state1.F)
     ΔmF = int(state2.mF - state1.mF)
     ΔP = int(state2.P - state1.P)
     
     flag_ΔP = np.abs(ΔP) != 2
     flag_ΔF = np.abs(ΔF) > 1
-    flag_ΔmF = np.abs(ΔmF) != ΔmF_allowed
-    flag_ΔFΔmF = (not flag_ΔmF) & (Fg == 0 & ΔF == 0 & ΔmF == 0)
+    flag_ΔmF = ΔmF != ΔmF_allowed
+    flag_ΔFΔmF = (not flag_ΔmF) & ( (ΔF == 0) & (ΔmF == 0) & (state1.mF == 0) )
     
     errors = ""
     if flag_ΔP:
