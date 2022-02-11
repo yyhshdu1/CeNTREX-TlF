508ad4e1f80810be7b0f6f8ffbb41aa122eb737d
ograsdijk <o.grasdijk@gmail.com>
2022-02-11 14:48:26 -0600

    add rich log handler

--- centrex_TlF/__init__.py
+++ centrex_TlF/__init__.py
@@ -7,3 +7,11 @@ from . import utils
 from . import transitions
 
 from .states.states import State, UncoupledBasisState, CoupledBasisState
+
+import logging
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="WARNING", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
