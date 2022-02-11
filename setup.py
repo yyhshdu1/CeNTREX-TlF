0ed136b3728e45025ed1a386aeb086b88036b2e3
ograsdijk <o.grasdijk@gmail.com>
2022-02-11 14:49:15 -0600

    black formatting on setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import setuptools
 
 setuptools.setup(
-    name = "CeNTREX_TlF",
-    author = "Olivier Grasdijk",
-    author_email = "olivier.grasdijk@yale.edu",
-    description = "general utility package for TlF molecular calculations used in the CeNTREX experiment",
-    url = "https://github.com/",
-    packages = setuptools.find_packages(),
-    install_requires = [
-        'numpy',
-        'sympy>=1.9',
-        'tqdm',
-        'julia',
-        ],
-    data_files = [('centrex_TlF/pre_calculated', 
-                    ['centrex_TlF/pre_calculated/transformation.db',
-                     'centrex_TlF/pre_calculated/uncoupled_hamiltonian_X.db',
-                     'centrex_TlF/pre_calculated/coupled_hamiltonian_B.db',
-                     'centrex_TlF/pre_calculated/matrix_elements.db',
-                     'centrex_TlF/pre_calculated/precalculated.json',
-                     'centrex_TlF/pre_calculated/transitions.pickle']),
-                    ('centrex_TlF/lindblad', ['centrex_TlF/lindblad/julia_common.jl'])],
-    python_requires = '>=3.6',
-    version = "0.1"
+    name="CeNTREX_TlF",
+    author="Olivier Grasdijk",
+    author_email="olivier.grasdijk@yale.edu",
+    description="general utility package for TlF molecular calculations used in the CeNTREX experiment",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    install_requires=["numpy", "sympy>=1.9", "tqdm", "julia", "rich"],
+    data_files=[
+        (
+            "centrex_TlF/pre_calculated",
+            [
+                "centrex_TlF/pre_calculated/transformation.db",
+                "centrex_TlF/pre_calculated/uncoupled_hamiltonian_X.db",
+                "centrex_TlF/pre_calculated/coupled_hamiltonian_B.db",
+                "centrex_TlF/pre_calculated/matrix_elements.db",
+                "centrex_TlF/pre_calculated/precalculated.json",
+                "centrex_TlF/pre_calculated/transitions.pickle",
+            ],
+        ),
+        ("centrex_TlF/lindblad", ["centrex_TlF/lindblad/julia_common.jl"]),
+    ],
+    python_requires=">=3.6",
+    version="0.1",
 )
